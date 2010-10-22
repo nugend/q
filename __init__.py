427edd4f700e3908d51871b6f1e0ad3f352f7abc
Daniel Nugent <nugend@gmail.com>
2010-10-22 09:30:15 -0700

    Importing time types for convenience into q namespace

--- __init__.py
+++ __init__.py
@@ -2,4 +2,5 @@ from collections import q_list as list
 from collections import table
 from collections import q_dict as dict
 from types import q_str as str
+from types import month, date, datetime, time, minute, second
 from conn import conn
